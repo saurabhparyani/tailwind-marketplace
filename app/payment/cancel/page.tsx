fa2cc1e919e4a891496432738188790660c79969
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-05-26 21:10:24 +0530

    fixed bug to deploy

--- app/payment/cancel/page.tsx
+++ app/payment/cancel/page.tsx
@@ -14,7 +14,7 @@ export default function CancelRoute() {
           <div className="mt-3 text-center sm:mt-5 w-full">
             <h3 className="text-lg leading-6 font-medium">Payment Canceled</h3>
             <p className="mt-2 text-sm text-muted-foreground">
-              Something went wrong with your payment. You haven't been charged.
+              Something went wrong with your payment. You have not been charged.
               Please try again.
             </p>
 
