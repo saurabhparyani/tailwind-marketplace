a4c40b0a2656b4da7da8b672d45d9fa4ae762434
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-05-26 21:18:44 +0530

    typo

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -4,14 +4,13 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 import {unstable_noStore as noStore} from 'next/cache'
 
-
 export async function GET(){
     noStore();
     const {getUser} = getKindeServerSession()
     const user = await getUser()
 
     if(!user || user === null || !user.id){
-        return new Error("Something went wrong")
+        throw new Error("Something went wrong")
     }
 
     let dbUser = await prisma.user.findUnique({
