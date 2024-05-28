0f8966114bd6d237fa0971a4cac1641ff054c974
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-05-28 16:24:33 +0530

    made products available to buy for users that are not signed up as well

--- app/actions.ts
+++ app/actions.ts
@@ -118,64 +118,55 @@ export async function UpdateUserSettings(prevState:any ,formData: FormData) {
 }
 
 export async function BuyProduct(formData: FormData) {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-
-    if (!user) throw new Error('Unauthorized');
-
     const id = formData.get("id") as string;
     const data = await prisma.product.findUnique({
-        where: {
-            id: id,
-        },
-        select: {
-            name: true,
-            smallDescription: true,
-            price: true,
-            images: true,
-            productFile: true,
-            User: {
-                select: {
-                    connectedAccountId: true
-                }
-            }
-        }
-    })
-
-    console.log('User creating session:', user);
-
-    const session = await stripe.checkout.sessions.create({
-        mode: 'payment',
-        line_items: [
-            {
-                price_data: {
-                    currency: 'inr',
-                    unit_amount: Math.round((data?.price as number) * 100),
-                    product_data: {
-                        name: data?.name as string,
-                        images: data?.images,
-                        description: data?.smallDescription,
-                    }
-                },
-                quantity: 1,
-            }
-        ],
-        metadata: {
-            link: data?.productFile as string,
-            userId: user.id
+      where: {
+        id: id,
+      },
+      select: {
+        name: true,
+        smallDescription: true,
+        price: true,
+        images: true,
+        productFile: true,
+        User: {
+          select: {
+            connectedAccountId: true,
           },
-        payment_intent_data: {
-            application_fee_amount: (Math.round((data?.price as number) * 100)) * 0.1,
-            transfer_data: {
-                destination: data?.User?.connectedAccountId as string,
-            }
         },
+      },
+    });
+  
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            unit_amount: Math.round((data?.price as number) * 100),
+            product_data: {
+              name: data?.name as string,
+              description: data?.smallDescription,
+              images: data?.images,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        link: data?.productFile as string,
+      },
+  
+      payment_intent_data: {
+        application_fee_amount: Math.round((data?.price as number) * 100) * 0.1,
+        transfer_data: {
+          destination: data?.User?.connectedAccountId as string,
+        },
+      },
 
         success_url: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/payment/success' : 'https://tailwind-marketplace.saurabhparyani.dev/payment/success',
         cancel_url: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/payment/cancel' : 'https://tailwind-marketplace.saurabhparyani.dev/payment/cancel'
     })
-
-    console.log('Created Stripe session:', session);
 
     return redirect(session.url as string)
 }
--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,14 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { stripe } from '@/app/lib/stripe';
-import { Resend } from 'resend';
-import ProductEmail from '@/app/components/ProductEmail';
-import prisma from '@/app/lib/db';
+import ProductEmail from "@/app/components/ProductEmail";
+import { stripe } from "@/app/lib/stripe";
+
 import { headers } from "next/headers";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   const body = await req.text();
+
   const signature = headers().get("Stripe-Signature") as string;
 
   let event;
@@ -19,49 +19,31 @@ export async function POST(req: NextRequest) {
       signature,
       process.env.STRIPE_SECRET_WEBHOOK as string
     );
-
   } catch (error: unknown) {
     return new Response("webhook error", { status: 400 });
   }
 
   switch (event.type) {
     case "checkout.session.completed": {
+      const session = event.data.object;
 
-    const session = event.data.object;
-    console.log('Received session:', session);
-
-    const userId = session.metadata?.userId;
-    console.log('Metadata userId:', userId);
-
-    // Retrieve the user's email from the database
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    });
-
-    console.log('Fetched user:', user);
-
-    if (user && user.email) {
       const link = session.metadata?.link;
-      console.log('Sending email to:', user.email);
 
-      // Send the email using the user's email
-      await resend.emails.send({
+      const { data, error } = await resend.emails.send({
         from: "Tailwind Marketplace <onboarding@resend.dev>",
-        to: user.email,
+        to: ["saurabhparyani64@gmail.com"],
         subject: "Your Product from Tailwind Marketplace is here 🎉",
         react: ProductEmail({
-          link: link as string,
+            link: link as string,
         }),
-      });
-    } else {
-      console.error('User email not found');
-  }
-    break;
-  }
-  default: {
-    console.log("unhandled event");
-  }
-}
+    });
 
-  return NextResponse.json({ received: true }, { status: 200 });
+      break;
+    }
+    default: {
+      console.log("unhandled event");
+    }
+  }
+
+  return new Response(null, { status: 200 });
 }
